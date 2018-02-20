aa7a8b2a8a75c3aff4f1416dc07d2193a54626c8
Astatine <astatine085@gmail.com>
2018-02-20 16:01:24 +0900

    rename offset -> bit_offset

--- include/binary_io/structure.hpp
+++ include/binary_io/structure.hpp
@@ -11,38 +11,38 @@ struct DummyElement {
   static constexpr Enum key = Enum::End;
 };
 // Element Iterator: end
-template <typename Enum, std::size_t offset_, typename...>
+template <typename Enum, std::size_t bit_offset_, typename...>
 struct ElementIterator {
   struct DummyIterator {};
   using element = DummyElement<Enum>;
   using next = DummyIterator;
-  static constexpr std::size_t offset = offset_;
+  static constexpr std::size_t bit_offset = bit_offset_;
   static constexpr bool is_end = true;
 };
 // Element Iterator
-template <typename Enum, size_t offset_, typename Head, typename... Tail>
-struct ElementIterator<Enum, offset_, Head, Tail...>:
-        private ElementIterator<Enum, offset_ + Head::bit_size, Tail...> {
+template <typename Enum, size_t bit_offset_, typename Head, typename... Tail>
+struct ElementIterator<Enum, bit_offset_, Head, Tail...>:
+        private ElementIterator<Enum, bit_offset_ + Head::bit_size, Tail...> {
   using element = Head;
-  using next = ElementIterator<Enum, offset_ + Head::bit_size, Tail...>;
-  static constexpr std::size_t offset = offset_;
+  using next = ElementIterator<Enum, bit_offset_ + Head::bit_size, Tail...>;
+  static constexpr std::size_t bit_offset = bit_offset_;
   static constexpr bool is_end = false;
 };
-// get offset
+// get bit offset
 template <typename Enum, Enum key, typename Iterator, bool is_end>
-struct get_offset;
-// get offset: end
+struct get_bit_offset;
+// get bit offset: end
 template <typename Enum, Enum key, typename Iterator>
-struct get_offset<Enum, key, Iterator, true> {
+struct get_bit_offset<Enum, key, Iterator, true> {
   static constexpr std::size_t value = 0;
 };
-// get offset: impl
+// get bit offset: impl
 template <typename Enum, Enum key, typename Iterator>
-struct get_offset<Enum, key, Iterator, false> {
+struct get_bit_offset<Enum, key, Iterator, false> {
   static constexpr std::size_t value =
         key == Iterator::element::key
-        ? Iterator::offset
-        : get_offset<
+        ? Iterator::bit_offset
+        : get_bit_offset<
                   Enum,
                   key,
                   typename Iterator::next,
@@ -76,17 +76,18 @@ class Structure {
   using element = get_element<key, Args...>;
 
   template <Enum key>
-  using offset_t = impl::get_offset<Enum, key, iterator, iterator::is_end>;
+  using bit_offset_t =
+          impl::get_bit_offset<Enum, key, iterator, iterator::is_end>;
 
   template <Enum key>
-  static std::size_t offset() {
+  static std::size_t bit_offset() {
     static_assert(key != Enum::End, "End is reserved");
     static_assert(element<key>::type::key != Enum::End, "invalid key");
-    return offset_t<key>::value;
+    return bit_offset_t<key>::value;
   }
 
-  static std::size_t size() {
-    return offset_t<Enum::End>::value;
+  static std::size_t bit_size() {
+    return bit_offset_t<Enum::End>::value;
   }
 };
 }  // namespace binary_io
--- test/structure.cpp
+++ test/structure.cpp
@@ -16,10 +16,10 @@ BOOST_AUTO_TEST_CASE(offset) {
           Element<Key, Key::Hoge, 1, uint8_t>,
           Element<Key, Key::Fuga, 3, uint8_t>,
           Element<Key, Key::Piyo, 4, uint8_t>>;
-  BOOST_CHECK_EQUAL(TestStructure::offset<Key::Hoge>(), 0);
-  BOOST_CHECK_EQUAL(TestStructure::offset<Key::Fuga>(), 1);
-  BOOST_CHECK_EQUAL(TestStructure::offset<Key::Piyo>(), 4);
-  BOOST_CHECK_EQUAL(TestStructure::size(), 8);
+  BOOST_CHECK_EQUAL(TestStructure::bit_offset<Key::Hoge>(), 0);
+  BOOST_CHECK_EQUAL(TestStructure::bit_offset<Key::Fuga>(), 1);
+  BOOST_CHECK_EQUAL(TestStructure::bit_offset<Key::Piyo>(), 4);
+  BOOST_CHECK_EQUAL(TestStructure::bit_size(), 8);
 }
 // empty
 BOOST_AUTO_TEST_CASE(empty) {
@@ -27,7 +27,7 @@ BOOST_AUTO_TEST_CASE(empty) {
     End,
   };
   using TestStructure = Structure<Key>;
-  BOOST_CHECK_EQUAL(TestStructure::size(), 0);
+  BOOST_CHECK_EQUAL(TestStructure::bit_size(), 0);
 }
 BOOST_AUTO_TEST_SUITE_END()  // structure
 }  // namespace binary_io

863d1755f7b01b4bf34abc99f7ebf25326e17230
Astatine <astatine085@gmail.com>
2018-02-20 16:29:45 +0900

    create Reader

---
+++ include/binary_io/reader.hpp
@@ -0,0 +1,46 @@
+#ifndef BINARY_IO_READER_HPP_
+#define BINARY_IO_READER_HPP_
+
+#include <cassert>
+#include <type_traits>
+#include "binary_io/structure.hpp"
+
+namespace binary_io {
+template<typename Structure>
+class Reader {
+  using structure = Structure;
+  using kind = typename structure::kind;
+
+  template<kind key>
+  using element = typename structure::template element<key>::type;
+
+ public:
+  // constructor
+  Reader(const void* buffer_head,
+         const std::size_t& buffer_size)
+    : buffer_head_(buffer_head),
+      buffer_size_(buffer_size) {
+    assert(buffer_head_ || buffer_size_ == 0);
+    assert(structure::bit_size() <= buffer_size_ * 8);
+  }
+  // getter
+  template<kind key>
+  typename element<key>::value_type Get() const {
+    static_assert(key != kind::End, "End is reserved");
+    static_assert(element<key>::key != kind::End, "invalid key");
+    const auto bit_offset = structure::template bit_offset<key>();
+    assert(buffer_head_
+           && (bit_offset + element<key>::bit_size <= buffer_size_ * 8));
+    // TODO: if (ptr & size check) when Element::default_value() is defined
+    return element<key>::Read(
+            buffer_head_,
+            buffer_size_,
+            structure::template bit_offset<key>());
+  }
+
+ private:
+  const void* buffer_head_;
+  std::size_t buffer_size_;
+};
+}  // namespace binary_io
+#endif  // BINARY_IO_READER_HPP_

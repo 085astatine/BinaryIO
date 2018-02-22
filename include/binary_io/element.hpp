dc404a997c93830c188039a33d95d70737e6f501
Astatine <astatine085@gmail.com>
2018-02-22 17:56:58 +0900

    declare functions that need redefinition in Element

--- include/binary_io/element.hpp
+++ include/binary_io/element.hpp
@@ -16,6 +16,20 @@ class Element {
   using value_type = Type;
   static constexpr Enum key = key_;
   static constexpr size_t bit_size = bit_size_;
+
+  // default value: be redefined with inheritance class
+  static constexpr value_type DefaultValue();
+  // read: be redefined with inheritance class
+  static value_type Read(
+          const void* buffer_head,
+          const std::size_t& buffer_size,
+          const std::size_t& bit_offset);
+  // write: be redefined with inheritance class
+  static void Write(
+          const void* buffer_head,
+          const std::size_t& buffer_size,
+          const value_type& value,
+          const std::size_t& bit_offset);
 };
 }  // namespace binary_io
 #endif  // BINARY_IO_ELEMENT_HPP_
